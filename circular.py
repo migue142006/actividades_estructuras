bde5291de3d9838dceb9464da7086cf12fb7a1b3
migue142006 <migue142006@gmail.com>
2025-10-20 17:00:14 -0500

    updt2
    Refactor Circular class methods for clarity and consistency.

--- circular.py
+++ circular.py
@@ -8,40 +8,38 @@ class Nodo:
 class Circular:
     def _init_(self):
         # Puntero al primer nodo de la lista 
-        self.primero = None # Contador de elementos para mostrar o recorrer
-        self.cont = 0
-    # Método para agregar un nuevo nodo al final de la lista circular
-    def agregar(self, dato):
-        # Se crea un nuevo nodo con el dato recibido
+        self.primero = None 
+        self.cont = 0 # Contador de elementos para mostrar o recorrer
+    
+    def agregar(self, dato): # Método para agregar un nuevo nodo al final de la lista
+
         nuevo = Nodo(dato)
-        # Si la lista está vacía (no hay primer nod)
-        if not self.primero:
-            # El nuevo nodo pasa a ser el primero
-            self.primero = nuevo
-            # Y su siguiente apunta a sí mismo (forma el círculo)
-            nuevo.siguiente = self.primero
-        # Si ya existen nodos en la lista
-        else:
+        if not self.primero: # Si la lista está vacía (no hay primer nod)
+            
+            self.primero = nuevo # El nuevo nodo pasa a ser el primero
+           
+            nuevo.siguiente = self.primero  # Y el nodo siguiente apunta a sí mismo (forma el círculo)
+
+        else:         # Si ya existen nodos en la lista
             
             actual = self.primero
             # Recorremos la lista hasta encontrar el último nodo
             while actual.siguiente != self.primero:
                 actual = actual.siguiente
-                # Incrementamos el contador al recorrer cada nodo
-                self.cont += 1
-            # Una vez en el último nodo, lo conectamos con el nuevo
-            actual.siguiente = nuevo
-            # Y el nuevo nodo apunta al primero, manteniendo la circularidad
-            nuevo.siguiente = self.primero
-            # Aumentamos el contador total de nodos
-            self.cont += 1
-    # este metodo lo utilizamos para mostrar todos los elementos de la lista circular
-    def mostrar(self):
-        # Comenzamos desde el primer nodo
+                
+                self.cont += 1 # Incrementamos el contador al recorrer cada nodo
+           
+            actual.siguiente = nuevo  # Una vez en el último nodo, lo conectamos con el nuevo
+            
+            nuevo.siguiente = self.primero # Y el nuevo nodo apunta al primero
+           
+            self.cont += 1  # Aumentamos el contador total de nodos
+  
+    def mostrar(self):   # metodo que muestra la lista
         actual = self.primero
         i = 0
-        # Recorremos los nodos mientras no hayamos pasado por todos
-        while actual and i < self.cont:
+  
+        while actual and i < self.cont:       # Recorremos los nodos mientras no hayamos pasado por todos
             # Mostramos el dato del nodo actual
             print(actual.dato)
             # Avanzamos al siguiente nodo
